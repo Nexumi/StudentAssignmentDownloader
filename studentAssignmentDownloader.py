dabc0f1bda60734b3225fd90595e8defe54df001
Nexumi <48866805+Nexumi@users.noreply.github.com>
2024-01-19 16:47:17 -0800

    Add files via upload

--- studentAssignmentDownloader.py
+++ studentAssignmentDownloader.py
@@ -1,13 +1,15 @@
 # https://canvasapi.readthedocs.io/en/stable/examples.html
 
 # Import the Canvas class
-import os
 from canvasapi import Canvas
+
+from shutil import copy2
 from zipfile import ZipFile
 from urllib.request import urlopen, urlretrieve
+from os import listdir, mkdir, chdir, remove, system, name
 
 def unzipper():
-    zips = os.listdir()
+    zips = listdir()
     i = 0
     while i < len(zips):
         if zips[i][-4:].lower() != ".zip":
@@ -15,28 +17,35 @@ def unzipper():
         else:
             i += 1
     for izip in zips:
-        os.mkdir(izip[:-4])
+        mkdir(izip[:-4])
         with ZipFile(izip, 'r') as zipObj:
           zipObj.extractall(path=izip[:-4])
-        os.remove(izip)
+        remove(izip)
 
-def header():
+def header(part):
     clear()
-    print("Welcome to Jimmy's Student Assignment Downloader!")
+    if part == 1:
+        print("Welcome to Jimmy's Student Assignment Downloader!")
+    elif part == 2:
+        print("Welcome to Jimmy's Student Rubric Generator!")
 
 def clear():
-    if os.name == "nt":
-        os.system("cls")
+    if name == "nt":
+        system("cls")
     else:
-        os.system("clear")
+        system("clear")
 
-def choice(values):
+def choice(values, part):
     x = -1
     while x < 0 or x >= i - 1:
-        header()
+        header(part)
         i = 1
         for value in values:
-            print(str(i) + ": " + str(value))
+            value = str(value)
+            try:
+                print(str(i) + ": " + value[:value.index(" (")])
+            except:
+                print(str(i) + ": " + value)
             i += 1
         try:
             x = int(input("Number: ")) - 1
@@ -44,36 +53,80 @@ def choice(values):
             pass
     return values[x]
 
+def get_rubrics():
+    data = urlopen("https://web.jpkit.us/grader-rubrics/rubrics.txt")
+    rubrics = []
+    for info in data:
+        rubrics.append(info.decode("utf-8").replace("\n", ""))
+    return choice(rubrics, 2)
+
+def generate_rubrics(assignment, names):
+    folder = assignment[:-5] + "s"
+    mkdir(folder)
+    chdir(folder)
+    urlretrieve("https://web.jpkit.us/grader-rubrics/" + assignment, assignment)
+    for name in names:
+        copy2(assignment, name + "-" + assignment)
+    remove(assignment)
+
 # Canvas API URL
 API_URL = "https://sfsu.instructure.com/"
 
+# Canvas API key
 try:
-	file = open("CanvasToken.txt", "x")
-	file.close()
-	input("Canvas Token not found! Please enter your Canvas Token into the CanvasToken.txt file then try again.")
+    file = open("CanvasToken.cfg", "x")
+    API_KEY = input("Canvas Token not found! Please enter your Canvas Token: ")
+    file.write(API_KEY)
+    file.close()
 except:
-	# Canvas API key
-	file = open("CanvasToken.txt")
-	API_KEY = file.read()
-	file.close()
+    file = open("CanvasToken.cfg")
+    API_KEY = file.read()
+    file.close()
 
-	# Initialize a new Canvas object
-	canvas = Canvas(API_URL, API_KEY)
+# Initialize a new Canvas object
+canvas = Canvas(API_URL, API_KEY)
 
-	# CSC 215 - Fall 2023
-	course = canvas.get_course(26589)
+# Get Assignment
+course = canvas.get_course(31427)
+assignments = course.get_assignments()
+assignment = choice(assignments, 1)
+submissions = assignment.get_submissions()
 
-	assignments = course.get_assignments()
+print("Downloading please wait...")
 
-	assignment = choice(assignments)
+# Generate Folder
+folder = str(assignment)
+folder = folder[:folder.index(" (")]
+try:
+    mkdir(folder)
+except:
+    i = 1
+    while True:
+        try:
+            mkdir(folder + " (" + str(i) + ")")
+            folder += " (" + str(i) + ")"
+            break
+        except:
+            i += 1
+chdir(folder)
 
-	submissions = assignment.get_submissions()
+# Getting Information
+names = []
+for submission in submissions:
+    if len(submission.attachments):
+        # Download Assignment
+        attachment = submission.attachments[0]
+        urlretrieve(attachment.url, attachment.filename)
 
-	os.mkdir(str(assignment))
-	os.chdir(str(assignment))
+        # Get Student Name
+        student = str(course.get_user(submission.user_id))
+        student = student[:student.index(" (")].replace(" ", "")
+        names.append(student)
+unzipper()
 
-	for submission in submissions:
-		if len(submission.attachments):
-			attachment = submission.attachments[0]
-			urlretrieve(attachment.url, attachment.filename)
-			unzipper()
+# Get Rubrics (For students that submitted)
+rubric = get_rubrics();
+print("Downloading please wait...")
+generate_rubrics(rubric, names)
+
+clear()
